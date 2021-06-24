bbb80f3e105c223e18ece905688316ce2b377523
ivel <ivelq3@gmail.com>
2021-06-24 18:14:37 +0300

    add filter set item value test

---
+++ backend/tests/test_filter_set_item_value.py
@@ -0,0 +1,30 @@
+import pytest
+from slugify import slugify
+from tests.factories import ProductAttributeValueFactory
+from products.services.filtering.filter_set_item_value import FilterSetItemValue, FilterSetItemValueBuilder
+
+
+@pytest.mark.django_db
+def test_filter_set_item_value_disable():
+    value = FilterSetItemValue(name="white", slug="white")
+    assert value.disabled == False
+    value.disable()
+    assert value.disabled == True
+
+
+@pytest.mark.django_db
+def test_filter_set_item_value_builder():
+    value1 = ProductAttributeValueFactory(name="синий")
+    value2 = ProductAttributeValueFactory(name="белый")
+
+    product_attribute_values_slugs = [value1.slug, value2.slug]
+
+    filter_set_item_values = FilterSetItemValueBuilder(product_attribute_values_slugs=product_attribute_values_slugs).execute()
+
+    for value in filter_set_item_values:
+        assert isinstance(value, FilterSetItemValue)
+        assert value.name == value.name.capitalize()
+        assert value.slug == slugify(value.name)
+        assert value.disabled == False
+
+    assert len(filter_set_item_values) == len(product_attribute_values_slugs)
